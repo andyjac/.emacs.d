ab76a6d4bdd9bab68d1c858f2ac2ef5d9339a3ae
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2017-01-13 17:08:34 -0800

    New main theme

--- my/my-theme.el
+++ my/my-theme.el
@@ -1,3 +1,8 @@
-(require 'vibrant-ink-theme)
+;; (require 'vibrant-ink-theme)
+;; (require 'base16-atelier-dune-theme)
+;; (require 'blackboard-theme)
+;; (require 'jbeans-theme)
+
+(require 'base16-default-dark-theme)
 
 (provide 'my-theme)
