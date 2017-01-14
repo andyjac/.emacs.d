d179fa27c09e2a2cc281f5e693bf3ea017b32cad
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2017-01-13 17:07:59 -0800

    Hella themes

---
+++ packages/realgud-1.4.3/realgud-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from realgud.el
+(define-package "realgud" "1.4.3" "A modular front-end for interacting with external debuggers" '((load-relative "1.2") (loc-changes "1.2") (test-simple "1.2.0")) :url "http://github.com/realgud/realgud/" :keywords nil)
---
+++ packages/ujelly-theme-20161222.534/ujelly-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ujelly-theme" "20161222.534" "Ujelly theme for GNU Emacs 24 (deftheme)" 'nil :url "http://github.com/marktran/color-theme-ujelly")
